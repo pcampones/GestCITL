18e0a6799c9cb0357005e9194e77f058b7cff9e9
Diana Jesus <2120346@my.ipleiria.pt>
2015-10-15 20:28:29 +0100

    Página Pagamentos criada

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -28,9 +28,10 @@
 	<div id="mainmenu">
 		<?php $this->widget('zii.widgets.CMenu',array(
 			'items'=>array(
-				array('label'=>'Home', 'url'=>array('/site/index')),
-				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
+				array('label'=>'Pagina Inicial', 'url'=>array('/site/index')),
+				array('label'=>'Sobre', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Email', 'url'=>array('/site/contact')),
+				array('label'=>'Pagamentos', 'url'=>array('/site/pagamentos')),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
---
+++ protected/views/site/pagamentos.php
@@ -0,0 +1,15 @@
+<?php
+/* @var $this SiteController */
+/* @var $model PagamentosForm */
+/* @var $form CActiveForm */
+
+$this->pageTitle=Yii::app()->name . ' - Pagamentos';
+$this->breadcrumbs=array(
+    'Pagamentos',
+);
+?>
+
+<h1>Pagamentos</h1>
+
+
+
